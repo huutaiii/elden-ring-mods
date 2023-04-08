dea095da260363a1c2fcdfd4000642d1f639584e
huutai <truongtai137@gmail.com>
2023-04-08 14:24:15 +0700

    ExtraMouseSensitivity: switch from fixed addresses to patterns  - add some documentation  - add logs

--- ExtraMouseSensitivity/dllmain.cpp
+++ ExtraMouseSensitivity/dllmain.cpp
@@ -5,6 +5,8 @@
 #include "INIReader.h"
 #pragma comment (lib,"../bin/MinHook.x64.lib")
 
+using ModUtils::MASK;
+
 struct FConfig
 {
     float SpeedMulH = 1.f;
@@ -23,10 +25,6 @@ struct FConfig
     }
 } Config;
 
-// offsets for 1.09 only
-constexpr ULONG_PTR OFFSET_FN_READ_H = 0xDEB730;
-constexpr ULONG_PTR OFFSET_FN_READ_V = 0xDEB7A0;
-
 float (*tram_GetMouseDeltaH)(LPVOID);
 float hk_GetMouseDeltaH(LPVOID p)
 {
@@ -39,6 +37,9 @@ float hk_GetMouseDeltaV(LPVOID p)
     return tram_GetMouseDeltaV(p) * Config.SpeedMulV;
 }
 
+// 48 8D 4D ?? E8 ???????? 0F28 F0 48 8D 4D ?? E8 ???????? F3 0F11 45 07 F3 0F11 75 0B
+std::vector<uint16_t> PATTERN_MOUSE_INPUT_CALLER = { 0x48, 0x8D, 0x4D, MASK, 0xE8, MASK, MASK, MASK, MASK, 0x0F, 0x28, 0xF0, 0x48, 0x8D, 0x4D, MASK, 0xE8, MASK, MASK, MASK, MASK, 0xF3, 0x0F, 0x11, 0x45, 0x07, 0xF3, 0x0F, 0x11, 0x75, 0x0B };
+
 DWORD WINAPI MainThread(LPVOID lpParams)
 {
     HMODULE mh_module = LoadLibraryA("mods\\bin\\MinHook.x64.dll");
@@ -50,16 +51,45 @@ DWORD WINAPI MainThread(LPVOID lpParams)
     MH_Initialize();
 
     Config.ReadINI();
+    ModUtils::Log("Using SpeedMulH = %f", Config.SpeedMulH);
+    ModUtils::Log("Using SpeedMulV = %f", Config.SpeedMulV);
 
     ULONG_PTR baseAddr = ModUtils::GetProcessBaseAddress(GetCurrentProcessId());
 
-    LPVOID pTargetH = reinterpret_cast<LPVOID>(baseAddr + OFFSET_FN_READ_H);
-    LPVOID pTargetV = reinterpret_cast<LPVOID>(baseAddr + OFFSET_FN_READ_V);
-    MH_CreateHook(pTargetH, &hk_GetMouseDeltaH, reinterpret_cast<LPVOID*>(&tram_GetMouseDeltaH));
-    MH_CreateHook(pTargetV, &hk_GetMouseDeltaV, reinterpret_cast<LPVOID*>(&tram_GetMouseDeltaV));
-    MH_QueueEnableHook(pTargetH);
-    MH_QueueEnableHook(pTargetV);
-    MH_ApplyQueued();
+    LPVOID pTargetH = nullptr;
+    LPVOID pTargetV = nullptr;
+
+    UINT_PTR pScan = ModUtils::SigScan(PATTERN_MOUSE_INPUT_CALLER);
+    if (pScan)
+    {
+        static constexpr SIZE_T CALL_SIZE = 5;
+        {
+            UINT_PTR CallH = pScan + 4;
+            UINT_PTR RelAddrH = *reinterpret_cast<UINT32*>(CallH + 1);
+            pTargetH = reinterpret_cast<LPVOID>(CallH + 5 + RelAddrH);
+
+            ModUtils::Log("Mouse input H module offset = %p", reinterpret_cast<UINT_PTR>(pTargetH) - baseAddr);
+        }
+
+        {
+            UINT_PTR CallV = pScan + 16;
+            UINT_PTR RelAddrV = *reinterpret_cast<UINT32*>(CallV + 1);
+            pTargetV = reinterpret_cast<LPVOID>(CallV + 5 + RelAddrV);
+
+            ModUtils::Log("Mouse input V module offset = %p", reinterpret_cast<UINT_PTR>(pTargetV) - baseAddr);
+        }
+    }
+
+    if (pTargetH && pTargetV)
+    {
+        MH_CreateHook(pTargetH, &hk_GetMouseDeltaH, reinterpret_cast<LPVOID*>(&tram_GetMouseDeltaH));
+        MH_CreateHook(pTargetV, &hk_GetMouseDeltaV, reinterpret_cast<LPVOID*>(&tram_GetMouseDeltaV));
+        MH_QueueEnableHook(pTargetH);
+        MH_QueueEnableHook(pTargetV);
+        MH_ApplyQueued();
+    }
+
+    ModUtils::CloseLog();
 
     return 0;
 }
